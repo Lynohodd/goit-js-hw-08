e64486afd89dad6b7023ddf174f50530564f375a
Henry <hennadii.lytvyn@gmail.com>
2025-03-11 22:57:19 +0000

    HW #8 done

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-function getElementWidth(content, padding, border) {
-  const elementWidth =
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2;
-  return elementWidth;
-}
